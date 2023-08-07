f4f488ed9e866bc3e3739be494b23f364e93e756
Pranay Garlapati <pgarlapati@zuora.com>
2023-08-07 20:58:41 +0530

    First commit : Producing msg into a topic through code

---
+++ src/main/java/com/github/pranay/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,28 @@
+package com.github.pranay.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        String bootStrapServer="localhost:9092";
+        Properties properties=new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer=new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String,String> record=new ProducerRecord<>("first-topic","hello world from code");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
